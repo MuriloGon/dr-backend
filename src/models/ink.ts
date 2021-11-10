c6eb19455ec5b86c81664861b4fbb733dc308bdc
Murilo Gonçalves <murilo.gon@live.com>
2021-11-10 17:29:12 -0300

    feat: implement inks model

---
+++ src/models/ink.ts
@@ -0,0 +1,57 @@
+import { getMongoRepository } from 'typeorm';
+import { ObjectId } from 'mongodb';
+import { Ink } from '../entity/Inks';
+
+
+export async function getAll() {
+  return getMongoRepository(Ink).find();
+}
+
+export async function getById(id: string): Promise<Ink | null> {
+  let validId: ObjectId | null;
+  try {
+    validId =  ObjectId(id);
+  } catch (err) {
+    return null;
+  }
+
+  const result = await getMongoRepository(Ink).findOne({ where: { _id: ObjectId(validId) } });
+  return result ? result : null;
+}
+
+export async function create(ink: Ink) {
+  const repo = getMongoRepository(Ink);
+  const result = repo.create(ink);
+  await repo.save(ink);
+  return result;
+}
+
+export async function editById(id:string, ink: Ink) {
+  let validId: ObjectId | null;
+  try {
+    validId =  ObjectId(id);
+  } catch (err) {
+    return null;
+  }
+
+  const repo = getMongoRepository(Ink);
+  const result = repo.update(ObjectId(validId), ink);
+  await repo.save(ink);
+
+  return result;
+}
+
+export async function deleteById(id) {
+  let validId: ObjectId | null;
+  try {
+    validId =  ObjectId(id);
+  } catch (err) {
+    return null;
+  }
+  const ink = await getById(id);
+  const repo = getMongoRepository(Ink);
+  const result = repo.delete(ObjectId(validId));
+  await repo.save(ink);
+
+  return result;
+}
